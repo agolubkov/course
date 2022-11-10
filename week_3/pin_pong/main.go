6bfbbeb1fbb250f9355ee3c8430c633813f7c465
Oleksii Golubkov <oleksii.golubkov@gmail.com>
2022-11-10 11:47:05 +0200

    Completed Ping-Pong task

--- week_3/pin_pong/main.go
+++ week_3/pin_pong/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+	"time"
 
+	"github.com/sirupsen/logrus"
+)
+
+func comm(id int, ch chan string) {
+	for {
+		select {
+		case mes := <-ch:
+			switch mes {
+			case "PING":
+				ch <- "PONG"
+				logrus.WithFields(logrus.Fields{"uid": id, "got": "PING", "sent": "PONG"}).Info("New Round")
+			case "PONG":
+				logrus.WithFields(logrus.Fields{"uid": id, "got": "PONG"}).Info("New Round")
+			}
+		case <-time.After(time.Millisecond * time.Duration(1000+rand.Intn(1000))):
+			ch <- "PING"
+			logrus.WithFields(logrus.Fields{"uid": id, "sent": "PING"}).Info("New Round")
+		}
+	}
+}
+
+func main() {
+	ch := make(chan string, 1)
+	go comm(1, ch)
+	go comm(2, ch)
+	fmt.Scanln()
 }
